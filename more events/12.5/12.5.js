f6da102ff4190819a1d0ce78d8c5227b340aef27
rajaaghanem <rajaaghanem@gmail.com>
2021-11-29 13:46:21 +0200

    smiles

---
+++ more events/12.5/12.5.js
@@ -0,0 +1,21 @@
+let txt = document.querySelector(".text");
+let btn = document.querySelector(".btn");
+let container = document.querySelector(".images");
+
+txt.addEventListener('keyup', (event)=>{
+    btn.addEventListener('click',(event)=>{
+        if (isNaN(txt.value))
+        alert("not a number!");
+     })
+
+     btn.addEventListener('click',(event)=>{
+        if (!isNaN(txt.value))
+        for(let i=0; i<txt.value; i++){
+            const img=document.createElement('img');
+            img.src="/1024px-Oxygen480-emotes-face-smile-big.svg.png";
+            img.style.width="100px";
+            container.appendChild(img);
+        }
+     })
+
+})
