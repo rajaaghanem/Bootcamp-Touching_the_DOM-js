691d7a21c61b28fc611e1e05dc206fd0367f3262
rajaaghanem <rajaaghanem@gmail.com>
2021-11-29 17:14:49 +0200

    lightbulb

--- more events/12.4/12.4.js
+++ more events/12.4/12.4.js
@@ -1,9 +1,10 @@
-const img = document.querySelector("img");
+const imgEl = document.querySelector("img");
 
-img.addEventListener('mouseover', (event)=>{
-    img.style.filter="invert(0)"
+imgEl.addEventListener('mouseover', (event)=>{
+    imgEl.style.filter="invert(0)"
 })
 
-img.addEventListener('mouseleave', (event)=>{
-    img.style.filter="invert(100%)"
+imgEl.addEventListener('mouseleave', (event)=>{
+    imgEl.style.filter="invert(100%)"
 })
+
