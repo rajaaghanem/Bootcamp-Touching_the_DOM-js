34856fde75628e5304a6010d131b7c152e98a6b4
rajaaghanem <rajaaghanem@gmail.com>
2021-11-30 15:36:47 +0200

    game

--- letter-guess/guess-letter.js
+++ letter-guess/guess-letter.js
@@ -1,11 +1,18 @@
 const gameBody = document.querySelector('body');
 const messages = document.querySelector(".messages");
-const lettersArray = ['a','b','c','d','e'];
+const hiddin = document.querySelector(".the-hiddin-letter");
+const keyGuessedP= document.querySelector(".key-guessed");
+const yesBtn = document.querySelector(".start-again");
+const shoWrongLettes = document.querySelector(".show-wrong-letters");
+
+// const lettersArray = ['a','b','c','d','e'];
+const lettersArray = ['a'];
 const wrongLetters = [];
 
 random = Math.floor(Math.random() * lettersArray.length);
 let secretLetter = lettersArray[random];
 let startFlag = true;
+yesBtn.style.visibility = "hidden";
 
 //check if the user input is a valid letter
 function isValidLetter(input){
@@ -19,17 +26,37 @@ function isLetterExist(input){
     
 }
 
+//check if the guess matches the secret letter
+function isMatch(input){
+   return (input == secretLetter)? true:false;
+}
+
 
 gameBody.addEventListener('keypress', (event)=>{
     const userInput = event.key;
-   if (isValidLetter(userInput)){   //the input is a letter
-     
-       if(isLetterExist(userInput)){ 
-        messages.innerText = `${userInput} has already been guessed`;
-        messages.style.color ="red";
-       }          
-       else wrongLetters.push(event.key);
-           
+   if (isValidLetter(userInput)){  //the input is a letter
+        
+        if(isMatch(userInput))  { //there is a match
+        hiddin.innerText = userInput; 
+        messages.innerText = "Right letter";
+        messages.style.color ="green";
+        keyGuessedP.innerText = "Would you like to play again?"
+        shoWrongLettes.innerText = "";
+        yesBtn.style.visibility = "visible";
+        
+       }
+
+       else{  // no match
+            if(isLetterExist(userInput)){  //already exist in wrong letters array
+                messages.innerText = `${userInput} has already been guessed`;
+                messages.style.color ="red";
+            }          
+            else{                         //not exist in wrong letters array
+                wrongLetters.push(event.key);
+                shoWrongLettes.innerText = wrongLetters;
+            } 
+       }
+                       
     } 
 
     else {                          //the input is NOT a letter
@@ -37,7 +64,4 @@ gameBody.addEventListener('keypress', (event)=>{
         messages.style.color ="red";
     }
     
-
 })
-
-console.log(wrongLetters);
