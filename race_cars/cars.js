c5f1ad100003946ffadc9b7935c3b6625aeb1719
rajaaghanem <rajaaghanem@gmail.com>
2021-12-01 23:13:28 +0200

    adding a function for car moving

--- race_cars/cars.js
+++ race_cars/cars.js
@@ -2,22 +2,29 @@ const car1 = document.querySelectorAll(".first");
 const car2 = document.querySelectorAll(".secound");
 
 const bodyGame = document.querySelector('body');
-let count1 =0;
-let count2 =0;
+let count1 =1;
+let count2 =1;
 console.dir(car1);
 
 bodyGame.addEventListener('keyup', (event)=>{
     if (event.key == 'ArrowRight'){
-        move(car1, count1);
-        count1++;
+        if (count1 < 11){
+            move(car1, count1);
+            count1++;
+        }
+        else  alert ("red car wins");
+
     }
     if(event.key == 'ArrowLeft'){
-        move(car2, count2)
-        count2++;
+        if (count2 < 11){
+            move(car2, count2)
+            count2++;
+        }
+        else  alert ("yellow car wins");
     }
 })
 
 function move(array, count){
-    array[count+1].classList.add('active');
-    array[count].classList.remove('active');
+    array[count].classList.add('active');
+    array[count-1].classList.remove('active');
 }
