c7839294eed538728579305b999bfb8212197dfa
rajaaghanem <rajaaghanem@gmail.com>
2021-12-05 12:43:47 +0200

    square 27.1

---
+++ prototypes/27.1-isSquare/27.1.js
@@ -0,0 +1,15 @@
+function Square(a, b, c, d){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+   }
+
+let sq = new Square(11,11,11,11);
+
+Square.prototype.isSquare = function (){
+    return (this.a === this.b && this.b===this.c && this.c===this.d)? true:false;
+}
+
+console.log(sq.isSquare());
+
