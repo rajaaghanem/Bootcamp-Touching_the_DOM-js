507d1d46f401f201654366afa7823c6a97a03614
rajaaghanem <rajaaghanem@gmail.com>
2021-12-06 14:00:01 +0200

    25.2

--- bind,call,apply/25.2/25.2.js
+++ bind,call,apply/25.2/25.2.js
@@ -3,12 +3,19 @@ const obj = {
     print1 : function(){
         console.log(this.name);
     },
-    print2: function(print1){
-        setTimeout(print1,30000)
+    print2 : function(print1){
+        setTimeout(print1, 200);
     }
+    // print2: function(){
+    //     setTimeout(function(){
+    //         console.log(this.name);
+    //     }.bind(this),200)
+    // }
 }
 
+
 obj.print1();
+// obj.print2();
 obj.print2(obj.print1.bind(obj));
 
 
