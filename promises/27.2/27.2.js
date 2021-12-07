6229cee26c3d45f394df3988417f63ed75ffd4c1
rajaaghanem <rajaaghanem@gmail.com>
2021-12-07 12:18:53 +0200

    done

--- promises/27.2/27.2.js
+++ promises/27.2/27.2.js
@@ -13,7 +13,7 @@ const makeAllCaps = (array)=>{
 const sortWords = (array) =>{
     return new Promise((resolve, reject)=>{
         for(let i=0; i< array.length; i++){
-            
+
             if(typeof array[i] !== "string" || (array[i].toUpperCase() === array[i].toLowerCase()))
             reject("it's not a string!");
         };
@@ -23,7 +23,7 @@ const sortWords = (array) =>{
 }
 
 
-let array1 = ["go", "AA" ,123, "hi"];
+let array1 = ["go", "AA" ,123, "22", "hi"];
 let array2 = ["hello", "bye", "no"];
 makeAllCaps(array2).then((data)=>sortWords(data).then((data)=> console.log(data)).catch((error)=> console.log(error)));
 makeAllCaps(array1).then((data)=>sortWords(data).then((data)=> console.log(data)).catch((error)=> console.log(error)));
