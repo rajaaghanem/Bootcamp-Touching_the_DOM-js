1baa552323956af88b670dc60b2567fe011db75c
rajaaghanem <rajaaghanem@gmail.com>
2021-12-07 12:12:17 +0200

    done

--- promises/27.2/27.2.js
+++ promises/27.2/27.2.js
@@ -1,7 +1,7 @@
 const makeAllCaps = (array)=>{
     return new Promise ((resolve, reject)=>{
         for(let i=0; i< array.length; i++){
-            if (array[i].toUpperCase() === array[i].toLowerCase())
+            if(typeof array[i] !== "string" || (array[i].toUpperCase() === array[i].toLowerCase()))
             reject("it's not a string!");
             array[i] = array[i].toUpperCase();
     };
@@ -13,7 +13,8 @@ const makeAllCaps = (array)=>{
 const sortWords = (array) =>{
     return new Promise((resolve, reject)=>{
         for(let i=0; i< array.length; i++){
-            if (array[i].toUpperCase() === array[i].toLowerCase())
+            
+            if(typeof array[i] !== "string" || (array[i].toUpperCase() === array[i].toLowerCase()))
             reject("it's not a string!");
         };
         array.sort();
@@ -21,7 +22,8 @@ const sortWords = (array) =>{
     })
 }
 
-let array1 = ["go", "1" , "hi"];
+
+let array1 = ["go", "AA" ,123, "hi"];
 let array2 = ["hello", "bye", "no"];
 makeAllCaps(array2).then((data)=>sortWords(data).then((data)=> console.log(data)).catch((error)=> console.log(error)));
 makeAllCaps(array1).then((data)=>sortWords(data).then((data)=> console.log(data)).catch((error)=> console.log(error)));
