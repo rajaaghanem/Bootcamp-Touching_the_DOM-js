da99fa98cd01949d9262f31924a2fd0c06256759
rajaaghanem <rajaaghanem@gmail.com>
2021-12-07 11:35:42 +0200

    27.2

---
+++ promises/27.2/27.2.js
@@ -0,0 +1,27 @@
+const  makeAllCaps = (array)=>{
+    return new Promise ((resolve, reject)=>{
+
+        for(let i=0; i< array.length; i++){
+            if (array[i].toUpperCase() === array[i].toLowerCase())
+            reject("it's not a string!");
+              array[i] = array[i].toUpperCase();
+    };
+        resolve(array);
+    })
+}
+
+const sortWords = (array) =>{
+    return new Promise((resolve, reject)=>{
+        // for(let i=0; i< array.length; i++){
+        //     if (array[i].toUpperCase() === array[i].toLowerCase())
+        //     reject("it's not a string!");
+        // };
+        array.sort();
+        resolve(array);
+    })
+}
+
+let array1 = ["go", "1" , "hi"];
+let array = ["hello", "bye", "no"];
+makeAllCaps(array).then((data)=>sortWords(data).then((data)=> console.log(data)).catch((error)=> console.log(error)));
+makeAllCaps(array1).then((data)=>sortWords(data).then((data)=> console.log(data)).catch((error)=> console.log(error)));
