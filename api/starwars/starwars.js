b0b3413724a5ea7c80f585529a6d6bc83fd1e2f6
rajaaghanem <rajaaghanem@gmail.com>
2021-12-08 16:06:47 +0200

    objects

--- api/starwars/starwars.js
+++ api/starwars/starwars.js
@@ -1,24 +1,33 @@
-const  charactersArray = [];
+const charactersArray = [];
 
-function creatTheArray(){
-    for(let i=0; i<10; i++){
-        getFetch(i+1);
-    }
-
+function creatTheArray() {
+  for (let i = 0; i < 10; i++) {
+    getFetch(i + 1);
+  }
 }
 
+async function getFetch(index) {
+  const res = await fetch(`https://swapi.dev/api/people/${index}/`);
+  const data = await res.json();
 
-async function getFetch(index){
-    const res = await fetch(`https://swapi.dev/api/people/${index}/`);
-    const data = await res.json();
-    // console.log(data);
-    charactersArray.push(data);
-    // return (data.PromiseResult);
+  const res2 = await fetch(`${data.homeworld}`);
+  const data2 = await res2.json();
+
+  console.log(data2);
+  generateArray(data, data2);
 }
 
-
-// charactersArray.push(getFetch(1));
-// getFetch(1);
+function generateArray(data, data2) {
+  let obj = {};
+  obj.name = data.name;
+  obj.hair = data.hair_color;
+  obj.height = data.height;
+  // console.log(data.homeworld);
+  obj.planet = {};
+  obj.planet.name = data2.name;
+  obj.planet.population = data2.population;
+  charactersArray.push(obj);
+  console.log(charactersArray);
+}
 
 creatTheArray();
-console.log(charactersArray);
