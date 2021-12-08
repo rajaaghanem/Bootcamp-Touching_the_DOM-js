7bd4f2ab71bb0e980430969442d9d3300acebf97
rajaaghanem <rajaaghanem@gmail.com>
2021-12-08 13:23:13 +0200

    refactoring

--- api/movies/movies.js
+++ api/movies/movies.js
@@ -4,9 +4,9 @@ const inputBtn = document.querySelector(".input");
 
 searchBtn.addEventListener("click", () => {
     card.innerHTML="";
+
     movieName = inputBtn.value;
     inputBtn.value = "";
-    // movieName = "game of thrones";
     movieName = movieName.split(" ").join('+');
     console.log(movieName);
 
@@ -33,26 +33,27 @@ function creatMovie(data){
     card.appendChild(title);
 
     const genre = document.createElement('p');
-    genre.innerText = data.Genre;
+    genre.innerText = `Genre: ${data.Genre}`;
     card.appendChild(genre);
 
     const year = document.createElement('p');
-    year.innerText = data.Year;
+    year.innerText = `Year: ${data.Year}`;
     card.appendChild(year);
 
     const plot =  document.createElement('p');
-    plot.innerText = data.Plot;
+    plot.innerText = `Plot: ${data.Plot}`;
     card.appendChild(plot);
 
     const director =  document.createElement('p');
-    director.innerText = data.Director;
+    director.innerText = `Director: ${data.Director}`;
     card.appendChild(director);
 
     const actors =  document.createElement('p');
-    director.innerText = data.Actors;
+    director.innerText = `Actors: ${data.Actors}`;
     card.appendChild(actors);
 
     const ratings = document.createElement('p');
+    ratings.innerText= "Rating: " 
     for(let i = 0; i<data.Ratings.length; i++){
         ratings.innerText += ` ${i+1}- the ${data.Ratings[i].Source} is: ${data.Ratings[i].Value} `;
     }
