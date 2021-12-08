759e05e78d49f523927d262a0bdeb29596641fdc
rajaaghanem <rajaaghanem@gmail.com>
2021-12-08 11:45:49 +0200

    adding a link

--- api/github_api/github.js
+++ api/github_api/github.js
@@ -10,7 +10,7 @@ searchBtn.addEventListener('click', ()=>{
     userName = inputBtn.value;
     inputBtn.value = "";
    
-    if(!arrayOfUsers.includes(userName)){
+    if(!arrayOfUsers.includes(userName) && userName!==""){
         arrayOfUsers.push(userName);
         getFetch(userName);
     }
@@ -19,25 +19,36 @@ searchBtn.addEventListener('click', ()=>{
 
 
 async function getFetch (searchName){
-    const res = await fetch(`https://api.github.com/users/${userName}`);
-    const data = await res.json();
-    // console.log(data)
-    creatingTheGit(data);
+    try{
+        const res = await fetch(`https://api.github.com/users/${userName}`);
+        console.log(res);
+        const data = await res.json();
+        console.log(data);
+        creatingTheGit(data);
+    }
+   catch(error){
+    console.log("invalid user name");
+   }
 }
 
 function creatingTheGit(data){
+    const link = document.createElement('a');
+    link.href = data.html_url;
+    link.setAttribute('target', '_blank');
+    contianer.appendChild(link);
+
     const avatar = document.createElement('img');
     avatar.src = data.avatar_url;
     avatar.width = "200";
     avatar.height = "200";
-    contianer.appendChild(avatar);
+    link.appendChild(avatar);
 
     const nameOf = document.createElement('div');
     nameOf.innerText = data.name;
-    contianer.appendChild(nameOf);
+    link.appendChild(nameOf);
 
     const publicRep = document.createElement('div');
     publicRep.innerText = data.public_repos;
-    contianer.appendChild(publicRep);
+    link.appendChild(publicRep);
 }
 
