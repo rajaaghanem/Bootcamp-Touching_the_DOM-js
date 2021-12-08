49aaa09eaffbeb1f837789456f044bc19d9427ec
rajaaghanem <rajaaghanem@gmail.com>
2021-12-08 11:51:15 +0200

    invalid user

--- api/github_api/github.js
+++ api/github_api/github.js
@@ -1,55 +1,48 @@
-const inputBtn = document.querySelector('.input');
-const searchBtn = document.querySelector('#searchBtn');
-const contianer = document.querySelector('.container');
+const inputBtn = document.querySelector(".input");
+const searchBtn = document.querySelector("#searchBtn");
+const contianer = document.querySelector(".container");
 
-let userName ="";
+let userName = "";
 
-let arrayOfUsers =[];
+let arrayOfUsers = [];
 
-searchBtn.addEventListener('click', ()=>{
-    userName = inputBtn.value;
-    inputBtn.value = "";
-   
-    if(!arrayOfUsers.includes(userName) && userName!==""){
-        arrayOfUsers.push(userName);
-        getFetch(userName);
-    }
-    
+searchBtn.addEventListener("click", () => {
+  userName = inputBtn.value;
+  inputBtn.value = "";
+
+  if (!arrayOfUsers.includes(userName) && userName !== "") {
+    arrayOfUsers.push(userName);
+    getFetch(userName);
+  }
 });
 
+async function getFetch(searchName) {
 
-async function getFetch (searchName){
-    try{
-        const res = await fetch(`https://api.github.com/users/${userName}`);
-        console.log(res);
-        const data = await res.json();
-        console.log(data);
-        if(res.ok) creatingTheGit(data);
-    }
-    
-   catch(error){
-    console.log("invalid user name");
-   }
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    if (res.ok) creatingTheGit(data);
+    else console.log ("invalid user");
 }
 
-function creatingTheGit(data){
-    const link = document.createElement('a');
-    link.href = data.html_url;
-    link.setAttribute('target', '_blank');
-    contianer.appendChild(link);
+function creatingTheGit(data) {
+  const link = document.createElement("a");
+  link.href = data.html_url;
+  link.setAttribute("target", "_blank");
+  contianer.appendChild(link);
 
-    const avatar = document.createElement('img');
-    avatar.src = data.avatar_url;
-    avatar.width = "200";
-    avatar.height = "200";
-    link.appendChild(avatar);
+  const avatar = document.createElement("img");
+  avatar.src = data.avatar_url;
+  avatar.width = "200";
+  avatar.height = "200";
+  link.appendChild(avatar);
 
-    const nameOf = document.createElement('div');
-    nameOf.innerText = data.name;
-    link.appendChild(nameOf);
+  const nameOf = document.createElement("div");
+  nameOf.innerText = data.name;
+  link.appendChild(nameOf);
 
-    const publicRep = document.createElement('div');
-    publicRep.innerText = data.public_repos;
-    link.appendChild(publicRep);
+  const publicRep = document.createElement("div");
+  publicRep.innerText = data.public_repos;
+  link.appendChild(publicRep);
 }
-
