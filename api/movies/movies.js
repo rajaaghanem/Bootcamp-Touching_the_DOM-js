c50d0177fb79d3fb7e52d6a2f87333e127ae0b0f
rajaaghanem <rajaaghanem@gmail.com>
2021-12-08 16:39:14 +0200

    refactoring

--- api/movies/movies.js
+++ api/movies/movies.js
@@ -17,44 +17,45 @@ async function  getFetch(movieName){
     const data= await res.json(); 
     if(data.Response === "True")
     creatMovie(data);
+    else console.log("Error");
     
 }
-
-function creatMovie(data){
+// data
+function creatMovie({Poster,Title, Genre, Year, Plot,Director, Actors, Ratings}){
     const poster = document.createElement("img");
-    poster.src = data.Poster;
+    poster.src = Poster;
     poster.width = "200";
     poster.height = "200";
     card.appendChild(poster);
 
     const title = document.createElement('h2');
-    title.innerText = data.Title;
+    title.innerText = Title;
     card.appendChild(title);
 
     const genre = document.createElement('p');
-    genre.innerText = `Genre: ${data.Genre}`;
+    genre.innerText = `Genre: ${Genre}`;
     card.appendChild(genre);
 
     const year = document.createElement('p');
-    year.innerText = `Year: ${data.Year}`;
+    year.innerText = `Year: ${Year}`;
     card.appendChild(year);
 
     const plot =  document.createElement('p');
-    plot.innerText = `Plot: ${data.Plot}`;
+    plot.innerText = `Plot: ${Plot}`;
     card.appendChild(plot);
 
     const director =  document.createElement('p');
-    director.innerText = `Director: ${data.Director}`;
+    director.innerText = `Director: ${Director}`;
     card.appendChild(director);
 
     const actors =  document.createElement('p');
-    director.innerText = `Actors: ${data.Actors}`;
+    director.innerText = `Actors: ${Actors}`;
     card.appendChild(actors);
 
     const ratings = document.createElement('p');
     ratings.innerText= "Rating: " 
     for(let i = 0; i<data.Ratings.length; i++){
-        ratings.innerText += ` ${i+1}- the ${data.Ratings[i].Source} is: ${data.Ratings[i].Value} `;
+        ratings.innerText += ` ${i+1}- the ${Ratings[i].Source} is: ${Ratings[i].Value} `;
     }
     card.appendChild(ratings);
 }
